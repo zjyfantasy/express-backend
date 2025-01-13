99b1f04c7c7cae13dbe924b366daa59e4a0be87b
zhongjinyan <zhongjinyan@biad.com.cn>
2025-01-13 11:08:42 +0800

    .env

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 const app = express();
 const port = 3000;
@@ -27,6 +28,39 @@ pool.getConnection((err, connection) => {
 // 示例路由
 app.get("/", (req, res) => {
   res.send("Hello from Express.js!");
+});
+
+app.get("/createTable", async (req, res) => {
+  // 简单查询
+  try {
+    const createTableSql = `
+  CREATE TABLE IF NOT EXISTS users (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   mobile VARCHAR(15) UNIQUE,
+   openId VARCHAR(30) UNIQUE,
+   email VARCHAR(255) NOT NULL UNIQUE
+  )  ENGINE=INNODB;
+ `;
+    const [results, fields] = await pool.execute(createTableSql);
+    console.log(results, fields); // 结果集
+    res.send("success");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// 示例路由
+app.get("/getUsers", async (req, res) => {
+  // 简单查询
+  try {
+    const [results, fields] = await pool.query("SELECT * FROM users");
+    res.status(200).send({ code: 1, data: results });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: err.message });
+  }
 });
 
 app.listen(port, () => {
