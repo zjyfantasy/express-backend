09d6613b5abaa924335d319b14caabf92391f5d5
zhongjinyan <zhongjinyan@biad.com.cn>
2025-01-13 14:17:58 +0800

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 const app = express();
 const port = 3000;
